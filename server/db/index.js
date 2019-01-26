d8d1046e2aacbbdf80e9e3cf8e64cd0329551721
Jake Ascher <ascherj@gmail.com>
2019-01-26 14:40:43 -0500

    Create and export mysql connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,22 @@
-var mysql = require('mysql');
-
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: ''
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
 
